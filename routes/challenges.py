9dc8ba6e8a4fe29a60ed555cf4a389b6aab20d26
polaris-labs <brave.tiger1227@gmail.com>
2021-05-07 06:37:37 -0400

    comment update api

--- routes/challenges.py
+++ routes/challenges.py
@@ -9,7 +9,6 @@ challenges_routes = Blueprint('challenges_routes', __name__)
 
 
 @challenges_routes.route('/', methods=["GET"])
-@jwt_required
 def get_challenges():
     get_all_challenges_command = GetAllChallengesCommand()
     result = get_all_challenges_command.execute()
@@ -41,25 +40,25 @@ def upload_challenges():
     return jsonify(result), 200
 
 
-@challenges_routes.route('/update', methods=["POST"])
-@jwt_required
-def update_challenges():
-    required_params = ["challenge_id", "name", "status", "rules"]
-    data = json.loads(request.data)
+# @challenges_routes.route('/update', methods=["POST"])
+# @jwt_required
+# def update_challenges():
+#     required_params = ["challenge_id", "name", "status", "rules"]
+#     data = json.loads(request.data)
 
-    if not all(elem in data.keys() for elem in required_params):
-        return jsonify(
-            {"status": "failed", "message": "Invalid input body. Expected keys :{0}".format(required_params)}), 400
+#     if not all(elem in data.keys() for elem in required_params):
+#         return jsonify(
+#             {"status": "failed", "message": "Invalid input body. Expected keys :{0}".format(required_params)}), 400
 
-    update_challenge_command = UpdateChallengeCommand()
-    update_challenge_command.input = {
-        "challenge_id": data.get("challenge_id"),
-        "name": data.get("name"),
-        "status": data.get("status"),
-        "description": data.get("description", None),
-        "rules": data.get("rules"),
-    }
-    result = update_challenge_command.execute()
-    if not update_challenge_command.successful:
-        return jsonify({'status': 'failed', 'messages': update_challenge_command.messages}), 400
-    return jsonify(result), 200
+#     update_challenge_command = UpdateChallengeCommand()
+#     update_challenge_command.input = {
+#         "challenge_id": data.get("challenge_id"),
+#         "name": data.get("name"),
+#         "status": data.get("status"),
+#         "description": data.get("description", None),
+#         "rules": data.get("rules"),
+#     }
+#     result = update_challenge_command.execute()
+#     if not update_challenge_command.successful:
+#         return jsonify({'status': 'failed', 'messages': update_challenge_command.messages}), 400
+#     return jsonify(result), 200
